fc087ff2a602f219ab138d3e454112d862cf7a23
mostafahelmy1259 <m83257065@gmail.com>
2025-03-08 22:34:41 +0200

    encrypt

--- caesar_cipher.py
+++ caesar_cipher.py
@@ -1,17 +1,29 @@
 def encrypt(text, key):
     result = ""
-    for i in range(len(message)):
-        #print(message[i])
-        start = ord('A') if message[i].isupper() else ord('a')
-        char = chr(start + (ord(message[i]) - start + key) % 26)
+    for i in range(len(text)):
+        start = ord('A') if text[i].isupper() else ord('a')
+        char = chr(start + (ord(text[i]) - start + key) % 26)
         result += char
+    return result  # Added return statement
+
+
+def decrypt(text, key):
+    result = ""
+    for i in range(len(text)):
+        start = ord('A') if text[i].isupper() else ord('a')
+        char = chr(start + (ord(text[i]) - start + key) % 26)
+        result += char
+    return result  # Added return statement
 
 
 message = input("Please enter a text: ")
 key = int(input("Please enter a key: "))
 cipher_method = input("Please choice to encrypt or decrypt: ")
 
-
 if cipher_method == "encrypt":
     encrypted_message = encrypt(message, key)
     print(encrypted_message)
+
+if cipher_method == "decrypt":
+    decrypted_message = decrypt(message, key)
+    print(decrypted_message)
